1465b93e837096b81d7f25b72480c06bebb5b6e9
Francesc Ferré Tarrés <vallcferre@gmail.com>
2020-12-30 17:27:31 +0100

    SpLog-DB: Add code for credentials database.

--- logsManager/dbConnection.py
+++ logsManager/dbConnection.py
@@ -1,13 +1,25 @@
 import mysql.connector
+import os
+import sys
+import json
 
+def readCredentials():
+    basedir = os.path.dirname(sys.modules['__main__'].__file__)
+    files = [f for f in os.listdir(basedir) if os.path.isfile(f)]
+    if 'credentialsDB.json' in files:
+        with open(basedir+'/credentialsDB.json') as credentials:
+            return json.load(credentials)
+    else:
+        raise IOError('File not found')
 
 def execute (query):
     try:
+        credentials = readCredentials()
         db_connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="logsSport"
+            host=credentials['host'],
+            user=credentials['user'],
+            password=credentials['password'],
+            database=credentials['database']
         )
 
         cursor = db_connection.cursor()
@@ -21,11 +33,12 @@ def execute (query):
 
 def save(query):
     try:
+        credentials = readCredentials()
         db_connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="logsSport"
+            host=credentials['host'],
+            user=credentials['user'],
+            password=credentials['password'],
+            database=credentials['database']
         )
         cursor = db_connection.cursor()
 
