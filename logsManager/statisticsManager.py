116412d907fa5cc61e6dd1d55bc4122e1ccf5f3c
Francesc Ferré Tarrés <vallcferre@gmail.com>
2020-12-31 15:23:30 +0100

    SpLog-DB: Add screen for statistics.

--- logsManager/statisticsManager.py
+++ logsManager/statisticsManager.py
@@ -1,15 +1,12 @@
 
-import tkinter
+import tkinter as tk
 import numpy as np
 import functools
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib.figure import Figure
 from eventListeners.screenActions import onCloseStatistics
-
-# llegir logs i estudiarlos
+from logsManager.dbConnection import *
 
 def statistics(screenStatisticsIcon, buttonStatistics):
-    screenStatistics = tkinter.Tk()
+    screenStatistics = tk.Tk()
     screenStatistics.wm_title("Sport log - ESTADÍSTIQUES")
     if screenStatisticsIcon:
         screenStatistics.iconbitmap(screenStatisticsIcon)
@@ -17,26 +14,23 @@ def statistics(screenStatisticsIcon, buttonStatistics):
     screenStatistics.focus_force()
     screenStatistics.geometry("700x500")
 
-    fig = Figure(figsize=(5, 4), dpi=100)
-    t = np.arange(0, 3, .01)
-    fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
-
-    canvas = FigureCanvasTkAgg(fig, master=screenStatistics)
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-    toolbar = NavigationToolbar2Tk(canvas, screenStatistics)
-    toolbar.update()
-    canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
-
-
-    def _quit():
-        buttonStatistics['state'] = 'normal'
-        screenStatistics.quit()
-        screenStatistics.destroy()
-
-
-    button = tkinter.Button(master=screenStatistics, text="Tancar", command=_quit)
-    button.pack(side=tkinter.BOTTOM)
+    frameInfoBySport = tk.Frame(screenStatistics)
+    tk.Label(screenStatistics, text="VEGADES REALITZADA CADA ACTIVITAT FÍSICA").pack(side=tk.TOP)
+    sports = execute('SELECT id, nom_esport FROM esport')
+    i=2
+    for sport in sports:
+        tk.Label(frameInfoBySport, text = sport[1]).grid(row=i, column=0)
+        tk.Label(frameInfoBySport, text = str(execute('SELECT COUNT(*) FROM esport_log WHERE esport = '+str(sport[0]))[0][0])).grid(row=i, column=1)
+        i= i + 1
+    
+    frameInfoBySport.pack(side=tk.TOP)
+    tk.Label(screenStatistics, text="VEGADES REALITZADA CADA ACTIVITAT FÍSICA PER DIFICULTAT").pack(side=tk.TOP)
+    frameInfoByDifficulty = tk.Frame(screenStatistics)
+    difficulties = execute('SELECT id, dificultat FROM dificultat')
+    i=2
+    for difficulty in difficulties:
+        tk.Label(frameInfoByDifficulty, text = difficulty[1]).grid(row=i, column=0)
+        tk.Label(frameInfoByDifficulty, text = str(execute('SELECT COUNT(*) FROM esport_log WHERE dificultat = '+str(difficulty[0]))[0][0])).grid(row=i, column=1)
+        i = i + 1
+    frameInfoByDifficulty.pack(side=tk.TOP)
     screenStatistics.protocol("WM_DELETE_WINDOW", functools.partial(onCloseStatistics, screenStatistics, buttonStatistics))
-    tkinter.mainloop()
